5c94042ab76f954c8f0d00bf27b117d94e7f45a1
Rafael Santana <rafaelns.br@gmail.com>
2024-04-28 01:02:53 -0300

    Responding to multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
 
-                match stream.write_all("+PONG\r\n".as_bytes()) {
-                    Ok(()) => println!("response sent"),
-                    Err(e) => println!("error sending response: {}", e),
+                match handle_connection(stream) {
+                    Ok(()) => println!("connection handled successfully"),
+                    Err(e) => println!("error handling connection: {}", e),
                 }
             }
             Err(e) => {
@@ -18,4 +18,16 @@ fn main() {
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), anyhow::Error> {
+    let buf = BufReader::new(&mut stream);
+
+    for line in buf.lines().collect::<Vec<_>>() {
+        let line = line?;
+        println!("line read: {}", &line);
+        stream.write_all("+PONG\r\n".as_bytes())?;
+    }
+
+    Ok(())
 }
