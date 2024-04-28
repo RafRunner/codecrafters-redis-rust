81af00c7bbf5d3c7b4e23aef3c889f56772cc191
Rafael Santana <rafaelns.br@gmail.com>
2024-04-28 00:37:40 -0300

    responding with pong

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                match stream.write_all("+PONG\r\n".as_bytes()) {
+                    Ok(()) => println!("response sent"),
+                    Err(e) => println!("error sending response: {}", e),
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
