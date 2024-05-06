95ea7ec1e63c68abd08e573a67271d8b5416d9a9
Rafael Santana <rafaelns.br@gmail.com>
2024-05-05 22:58:58 -0300

    feat: implementing key expiration via the px option on the set command

--- src/redis_command.rs
+++ src/redis_command.rs
@@ -1,52 +1,44 @@
+use std::time::Duration;
+
 use crate::redis_type::RedisType;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum RedisCommand {
     PING,
     ECHO(String),
-    SET { key: String, val: RedisType },
-    GET { key: String },
+    SET {
+        key: String,
+        val: RedisType,
+        ttl: Option<Duration>,
+    },
+    GET {
+        key: String,
+    },
 }
 
 impl RedisCommand {
     pub fn parse(data: &RedisType) -> Option<RedisCommand> {
         match data {
-            RedisType::List { data } if !data.is_empty() => match data.len() {
-                1 => Self::parse(&data[0]),
-                2 => Self::parse_two_element_command(data),
-                3 => Self::parse_three_element_command(data),
-                _ => None,
-            },
+            RedisType::List { data } if !data.is_empty() => {
+                if data.len() == 1 {
+                    Self::parse(&data[0])
+                } else {
+                    match Self::extract_string(&data[0]) {
+                        Some(command) => match command.to_lowercase().as_str() {
+                            "echo" => Self::parse_echo(&data[1..]),
+                            "get" => Self::parse_get(&data[1..]),
+                            "set" => Self::parse_set(&data[1..]),
+                            _ => None,
+                        },
+                        None => None,
+                    }
+                }
+            }
             RedisType::BulkString { data, .. } | RedisType::SimpleString { data, .. } => {
                 match data.to_lowercase().as_str() {
                     "ping" => Some(RedisCommand::PING),
                     _ => None,
                 }
-            }
-            _ => None,
-        }
-    }
-
-    fn parse_two_element_command(data: &[Box<RedisType>]) -> Option<RedisCommand> {
-        let command = Self::extract_string(&data[0])?.to_lowercase();
-        let argument = Self::extract_string(&data[1])?;
-
-        match command.as_str() {
-            "echo" => Some(RedisCommand::ECHO(argument.to_string())),
-            "get" => Some(RedisCommand::GET {
-                key: argument.to_string(),
-            }),
-            _ => None,
-        }
-    }
-
-    fn parse_three_element_command(data: &[Box<RedisType>]) -> Option<RedisCommand> {
-        let command = Self::extract_string(&data[0])?.to_lowercase();
-        match command.as_str() {
-            "set" => {
-                let key = Self::extract_string(&data[1])?.to_string();
-                let val = data[2].as_ref().clone();
-                Some(RedisCommand::SET { key, val })
             }
             _ => None,
         }
@@ -58,11 +50,74 @@ impl RedisCommand {
             _ => None,
         }
     }
+
+    fn parse_echo(data: &[Box<RedisType>]) -> Option<RedisCommand> {
+        data.get(0).and_then(|argument| {
+            Self::extract_string(argument).map(|argument| RedisCommand::ECHO(argument.to_string()))
+        })
+    }
+
+    fn parse_get(data: &[Box<RedisType>]) -> Option<RedisCommand> {
+        data.get(0).and_then(|key| {
+            Self::extract_string(key).map(|key| RedisCommand::GET {
+                key: key.to_string(),
+            })
+        })
+    }
+
+    fn parse_set(data: &[Box<RedisType>]) -> Option<RedisCommand> {
+        if data.len() < 2 {
+            return None;
+        }
+
+        let key = Self::extract_string(&data[0])?.to_string();
+        let value = data[1].as_ref().clone();
+        let mut ttl: Option<Duration> = None;
+
+        // Process optional parameters
+        let mut i = 2;
+        while i < data.len() {
+            if let Some(arg) = Self::extract_string(&data[i]) {
+                match arg.to_uppercase().as_str() {
+                    "PX" => {
+                        ttl = data.get(i + 1).and_then(|val| {
+                            Self::extract_string(val)
+                                .and_then(|v| v.parse::<u64>().ok())
+                                .map(|ttl| Duration::from_millis(ttl))
+                        });
+                        i += 2; // Skip the next item since it's part of this option
+                    }
+                    _ => i += 1,
+                }
+            } else {
+                i += 1;
+            }
+        }
+
+        Some(RedisCommand::SET {
+            key,
+            val: value,
+            ttl,
+        })
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn create_list(strings: &[&str]) -> RedisType {
+        RedisType::List {
+            data: strings
+                .iter()
+                .map(|elem| {
+                    Box::new(RedisType::BulkString {
+                        data: elem.to_string(),
+                    })
+                })
+                .collect(),
+        }
+    }
 
     #[test]
     fn test_parse_ping() {
@@ -102,16 +157,7 @@ mod tests {
 
     #[test]
     fn test_parse_invalid() {
-        let data = RedisType::List {
-            data: vec![
-                Box::new(RedisType::BulkString {
-                    data: "invalid".to_string(),
-                }),
-                Box::new(RedisType::BulkString {
-                    data: "world".to_string(),
-                }),
-            ],
-        };
+        let data = create_list(&["invalid", "world"]);
 
         let result = RedisCommand::parse(&data);
         assert_eq!(result, None);
@@ -126,45 +172,37 @@ mod tests {
         let result = RedisCommand::parse(&data);
         assert_eq!(result, None);
     }
-
+ 
     #[test]
     fn test_set_command() {
-        let set = RedisType::List {
-            data: vec![
-                Box::new(RedisType::SimpleString {
-                    data: "SET".to_string(),
-                }),
-                Box::new(RedisType::SimpleString {
-                    data: "mykey".to_string(),
-                }),
-                Box::new(RedisType::BulkString {
-                    data: "myvalue".to_string(),
-                }),
-            ],
-        };
+        let set = create_list(&["SET", "mykey", "myvalue"]);
         assert_eq!(
             RedisCommand::parse(&set),
             Some(RedisCommand::SET {
                 key: "mykey".to_string(),
                 val: RedisType::BulkString {
-                    data: "myvalue".to_string()
-                }
+                    data: "myvalue".to_string(),
+                },
+                ttl: None
+            })
+        );
+
+        let set_with_expiry = create_list(&["SET", "MyKeyTwo", "OtherValue", "px", "200"]);
+        assert_eq!(
+            RedisCommand::parse(&set_with_expiry),
+            Some(RedisCommand::SET {
+                key: "MyKeyTwo".to_string(),
+                val: RedisType::BulkString {
+                    data: "OtherValue".to_string(),
+                },
+                ttl: Some(Duration::from_millis(200))
             })
         );
     }
 
     #[test]
     fn test_get_command() {
-        let get = RedisType::List {
-            data: vec![
-                Box::new(RedisType::SimpleString {
-                    data: "GET".to_string(),
-                }),
-                Box::new(RedisType::SimpleString {
-                    data: "mykey".to_string(),
-                }),
-            ],
-        };
+        let get = create_list(&["GET", "mykey"]);
         assert_eq!(
             RedisCommand::parse(&get),
             Some(RedisCommand::GET {
--- src/redis_runtime.rs
+++ src/redis_runtime.rs
@@ -1,10 +1,16 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Instant};
 
 use crate::{redis_command::RedisCommand, redis_type::RedisType};
 
 #[derive(Debug)]
+struct ValueWithExpiry {
+    value: RedisType,
+    expiry: Option<Instant>,
+}
+
+#[derive(Debug)]
 pub struct RedisRuntime {
-    values: HashMap<String, RedisType>,
+    values: HashMap<String, ValueWithExpiry>,
 }
 
 impl RedisRuntime {
@@ -20,17 +26,32 @@ impl RedisRuntime {
                 data: "PONG".to_string(),
             },
             RedisCommand::ECHO(payload) => RedisType::BulkString { data: payload },
-            RedisCommand::SET { key, val } => {
-                self.values.insert(key, val);
+            RedisCommand::SET { key, val, ttl } => {
+                self.values.insert(
+                    key,
+                    ValueWithExpiry {
+                        value: val,
+                        expiry: ttl.map(|ttl| Instant::now() + ttl),
+                    },
+                );
 
                 RedisType::SimpleString {
                     data: "OK".to_string(),
                 }
             }
-            RedisCommand::GET { key } => match self.values.get(&key) {
-                Some(val) => val.clone(),
-                None => RedisType::NullBulkString,
-            },
+            RedisCommand::GET { key } => {
+                if let Some(val_with_expiry) = self.values.get(&key) {
+                    if let Some(expiry) = val_with_expiry.expiry {
+                        if Instant::now() > expiry {
+                            self.values.remove(&key);
+                            return RedisType::NullBulkString;
+                        }
+                    }
+                    return val_with_expiry.value.clone();
+                }
+
+                RedisType::NullBulkString
+            }
         }
     }
 }
@@ -77,6 +98,7 @@ mod tests {
             val: RedisType::BulkString {
                 data: "value1".to_string(),
             },
+            ttl: None,
         });
         assert_eq!(
             result,
@@ -86,7 +108,7 @@ mod tests {
         );
 
         // Ensure the value is actually set
-        let value = runtime.values.get("key1").unwrap();
+        let value = &runtime.values.get("key1").unwrap().value;
         assert_eq!(
             value,
             &RedisType::BulkString {
@@ -100,8 +122,11 @@ mod tests {
         let mut runtime = RedisRuntime::new();
         runtime.values.insert(
             "key1".to_string(),
-            RedisType::BulkString {
-                data: "value1".to_string(),
+            ValueWithExpiry {
+                value: RedisType::BulkString {
+                    data: "value1".to_string(),
+                },
+                expiry: None,
             },
         );
 
